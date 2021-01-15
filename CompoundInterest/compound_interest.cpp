ce888b9476393207d5d9ff9b7430f22a08ec3423
Ae <aepzgar@gmail.com>
2021-01-14 21:36:18 -0500

    references over pointers

--- CompoundInterest/compound_interest.cpp
+++ CompoundInterest/compound_interest.cpp
@@ -8,55 +8,57 @@
  * e = euler constant -- C++ std::exp()
  *
  *
- *This program uses pointers -- another program will be done using references i.e. & 
+ *This program uses vectors and pass-by reference.
+ *If there are any errors or fixes, please let me know. 
  */
 
 #include <iostream> 
 #include <cmath>
+#include <vector>
 
 class CompInterest {
 
   public:
 
 
-  void t(double *A, double *P, double *r) { 
-	  double t[4];
+  void t(std::vector<double> &A, std::vector<double> &P, std::vector<double> &r) { 
+	  std::vector<double> t;
 	  for(int i = 0; i< 4; i++) { 
-		  t[i] = std::log(A[i]/P[i]) / r[i]; 
+		  t.push_back(std::log(A[i]/P[i]) / r[i]); 
 	  }
 	  for(int i = 0; i< 4; i++) { 
 		  std::cout << std::abs(t[i]) << std::endl; 
 	  }
   }
 
-  void r(double *A, double *P, double *t) { 
-	  double r[4];
+  void r(std::vector<double> &A,std::vector<double> &P,std::vector<double> &t) { 
+	  std::vector<double> r;
 	  for(int i = 0; i< 4; i++) { 
-		  r[i] = std::log(A[i]/P[i]) / t[i]; 
+		  r.push_back(std::log(A[i]/P[i]) / t[i]); 
 	  }
 	  for(int i = 0; i < 4; i++) { 
 		  std::cout << std::abs(r[i]) << std::endl; 
 	  }
 	}
 
-  void principal_value(double *A, double *r, double *t){
-	  double P[4];
-	  for(int i = 0; i< 4; i++) { 
-		  double r_t [4]; 
-		  r_t[i] = r[i] * t[i]; 
-		  P[i] = A[i]/std::exp(r_t[i]); 
+  void principal_value(std::vector<double> &A, std::vector<double> &r, std::vector<double> &t){
+	  std::vector<double> P;
+	  std::vector<double> r_t;
+	  for(int i = 0; i< 4; i++) { 		  
+		  r_t.push_back(r[i] * t[i]); 
+		  P.push_back(A[i]/std::exp(r_t[i])); 
 	  }
 	  for(int i = 0; i<4; i++) { 
 		  std::cout << P[i] << std::endl; 
 	  }
   }
 
-  void end_value(double *P, double *r, double *t) {
-	  double A[4]; 
-	  for(int i = 0; i < 4; i++) {
-		  double r_t [4];
-		  r_t [i] = r[i] * t[i]; 
-		  A [i] = P[i] * std::exp(r_t[i]); 
+  void end_value(std::vector<double> &P, std::vector<double> &r, std::vector<double> &t) {
+	  std::vector<double> A; 
+	  std::vector<double> r_t;
+	  for(int i = 0; i < 4; i++) {	
+		  r_t.push_back(r[i] * t[i]); 
+		  A.push_back(P[i] * std::exp(r_t[i])); 
 	  }
 	  for(int i = 0; i < 4; i++) { 
 		  std::cout << A[i] <<std::endl; 
@@ -69,12 +71,19 @@ int main() {
 
   CompInterest end; 
 
-  double P [] = {13.8, 10.0, 13.22, 12.00}; 
-  //double r [] = {0.05, 0.80, 0.23, 0.123}; 
-  double t [] = {10, 23, 4, 5};
-  double A [] = {22.44, 23, 54, 8.332}; 
-  //end.end_value(P, r, t);
-  end.r(A, P, t); 
+  std::vector<double> A = {22.75, 10.0, 13.22, 12.00}; 
+  std::vector<double> r= {0.05, 0.80, 0.23, 0.123}; 
+  std::vector<double> t = {10, 23, 4, 5};
+  //std::vector<double> P = {13.8, 23, 54, 8.332};
+  
+  end.principal_value(A,r,t); 
+
+  //Working Properly 
+  //end.t(A,P,r);
+  //end.r(A, P, t);
+  //end.end_value(P,r,t);
+  //end.principal_value(A,r,t); 
+
 
   return 0; 
 }
