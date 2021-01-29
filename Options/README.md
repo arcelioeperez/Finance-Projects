d1b5872f4670eff808ff819d855214c5962d79bf
arcelioeperez <aepzgar@gmail.com>
2021-01-29 14:59:17 -0500

    new files

---
+++ Options/README.md
@@ -0,0 +1,26 @@
+# Options Basics  
+
+Variables:  
+- S - current price  
+- E - strike price (future price)  
+- T - time 
+
+Call Options:  
+
+"The purchaser has the legal rigtht to decide what will be done" - Donald G. Sarri, Mathematics of Finance, Springer.   
+
+Profit Formula  
+> Call(S, T) = max(0, S - (E + Premium Paid))    
+
+Put Options:  
+
+"With a Put, the benefit and rights belong to the seller" - Donald G. Sarri, Mathematics of
+Finance, Springer.  
+
+Profit Formula  
+> Put(S, T) = max(0, (E + Premium Paid) - S)  
+
+More information here:  
+[Options](https://www.investopedia.com/options-basics-tutorial-4583012)  
+[The Greeks: Delta, Theta, Gamma, Vega, and Rho](https://www.investopedia.com/terms/g/greeks.asp)  
+[Youtube Options](https://www.youtube.com/watch?v=SD7sw0bf1ms)  
---
+++ Options/short.py
@@ -0,0 +1,53 @@
+"""
+Short Selling Script
+
+Price of the underlying stock goes up: 
+- Margin call 
+
+Price goes down: **to do** 
+- Money to the seller's account
+"""
+
+import pandas as pd
+
+class Short: 
+    def __init__(self, shares, price, mfee=0.3): 
+        self.shares = shares
+        self.price = price
+        self.mfee = mfee 
+    def price_increase(self, end, steps=5):
+        self.end = end 
+        self.steps = steps
+        principal = self.shares * self.price
+        margin_amount = 0.5 * principal 
+        initial_req = margin_amount + principal 
+        short_value = 0
+        margin_call = 0
+        total_req = 0
+     
+        data = pd.DataFrame([]) 
+
+        for i in range(self.price, self.end, self.steps): 
+            short_value = self.shares * i 
+            margin_req = short_value * self.mfee
+            total_req = short_value + margin_req
+            if total_req < initial_req: 
+                margin_call = 0
+                data = data.append(pd.DataFrame({"Shares": [self.shares],
+                    "Share Price": [i], 
+                "Short Sale Value": [short_value], "Maintenance Margin":
+                [margin_req], "Total Req": [total_req], "Margin Call":
+                [margin_call]}), ignore_index = True ) 
+            else: 
+                margin_call = total_req - initial_req
+                data = data.append(pd.DataFrame({"Shares": [self.shares],
+                    "Share Price": [i], 
+                "Short Sale Value": [short_value], "Maintenance Margin":
+                [margin_req], "Total Req": [total_req], "Margin Call":
+                [margin_call]}), ignore_index = True ) 
+
+        print(data) 
+# Test
+test = Short(shares = 1000, price = 50,  mfee = 0.3) 
+ans = test.price_increase(end = 80, steps = 5)
+print(ans)     
