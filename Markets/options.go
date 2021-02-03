4f7a800f52950f38f46c9a6ca8908886205ec5f5
arcelioeperez <aepzgar@gmail.com>
2021-02-03 10:18:07 -0500

    formatted options.go emacs

--- Markets/options.go
+++ Markets/options.go
@@ -20,16 +20,16 @@ func puts(S, E, cprice float64) float64 {
 
 func main() {
 	//Call test 
+	var Sc float64 = 110.00
+   	var Ec float64 = 90.00
+    	var cpricec float64 = 1.20
 
-    var Sc float64 = 110.00
-    var Ec float64 = 90.00
-    var cpricec float64 = 1.20
+    	cans := calls(Sc, Ec, cpricec)
 
-    cans := calls(Sc, Ec, cpricec)
-
-    fmt.Printf("Call Profitability: %.2f ", cans)
+    	fmt.Printf("Call Profitability: %.2f ", cans)
 	fmt.Printf("\n")
 
+	//Put test
 	var S float64 = 90.00
 	var E float64 = 100.00
 	var cprice float64 = 2.00
