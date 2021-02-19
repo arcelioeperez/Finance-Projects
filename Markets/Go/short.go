8d18d4e385a0a71e9dfe7b7ba6f3e2f2a835c40e
arcelioeperez <aepzgar@gmail.com>
2021-02-19 12:48:27 -0500

    short.go price decrease

--- Markets/Go/short.go
+++ Markets/Go/short.go
@@ -73,6 +73,35 @@ func price_increase(end float64, steps float64, mfee float64, shares float64, pr
 	fmt.Println(df.Table())
 }
 
+func price_decrease(end float64, steps float64, mfee float64, shares float64, price float64) {
+  var principal float64 = shares * price
+  var margin_amount float64 = 0.5 * principal
+  var initial_req float64 = margin_amount + principal
+  var mrel float64 = 0
+
+  sval := dataframe.NewSeriesFloat64("Shares", nil, shares)
+  share_price := dataframe.NewSeriesFloat64("Share Price",nil,price)
+  short_val := dataframe.NewSeriesFloat64("Short Sale Value",nil,  principal)
+  margin_val := dataframe.NewSeriesFloat64("Additional Value",nil,  margin_amount)
+  total_requirement := dataframe.NewSeriesFloat64("Total Requirement",nil,initial_req)
+  mreleased := dataframe.NewSeriesFloat64("Margin Released", nil, mrel)
+
+  for i := price - steps; i > end; i-= steps {
+	var short_value float64 = shares * i
+	var margin_req float64 = 0.5 * short_value
+	var total_req float64 = short_value + margin_req
+	mrel = initial_req - total_req
+	sval.Append(shares)
+	share_price.Append(i)
+	short_val.Append(short_value)
+	margin_val.Append(margin_req)
+	total_requirement.Append(total_req)
+	mreleased.Append(mrel)
+  }
+
+  df := dataframe.NewDataFrame(sval, share_price, short_val, margin_val, total_requirement, mreleased)
+  fmt.Println(df.Table())
+}
 
 func main() {
 	/*
@@ -82,12 +111,16 @@ func main() {
 
 	fmt.Println("First Transaction: ", ans)
 	*/
+	//Struct 
+	//price, shares, mfee, end, steps
 
 	//Price Increase
 	stock := Stock{50.00, 1000.00, 0.3, 80.00, 5.00}
 
 	price_increase(stock.end, stock.steps, stock.mfee, stock.shares, stock.price)
 
-	//Price Decrease -- to do ...
+	//Price Decrease 
+	stock2 := Stock{50.00, 1000.00, 0.3, 30.00, 5.00}
+	price_decrease(stock2.end, stock2.steps, stock2.mfee, stock2.shares, stock2.price)
 
 }
